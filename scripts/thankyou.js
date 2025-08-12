a62e409964aa1bf212dd7c3e4dae33319896d20a
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-08-11 23:24:36 -0600

    commented out console.log statements in thankyou.js

--- scripts/thankyou.js
+++ scripts/thankyou.js
@@ -2,12 +2,12 @@ const form = document.querySelector('.join-form');
 
 const params = new URLSearchParams(window.location.search);
 
-console.log(params.get('first-name'));
-console.log(params.get('last-name'));
-console.log(params.get('email'));
-console.log(params.get('phone'));
-console.log(params.get('email-frequency'))
-console.log(params.get('timestamp'));
+// console.log(params.get('first-name'));
+// console.log(params.get('last-name'));
+// console.log(params.get('email'));
+// console.log(params.get('phone'));
+// console.log(params.get('email-frequency'))
+// console.log(params.get('timestamp'));
 
 document.querySelector('#form-results').innerHTML =
     `<p>Here is the data you entered:</p>
